989c6e80e7de1e3921885af4039aaf6b4d17c25e
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2021-04-06 23:43:33 +0900

    line breaks for readability

--- src/index.js
+++ src/index.js
@@ -84,8 +84,10 @@ export function All(...rules) {
     let $cur = $;
     for (let i = 0; i < rules.length; i++) {
       const $before = i > 0 ? skipIgnored($cur) : $cur;
+
       const $after = rules[i]($before);
       if ($after === $before) return $;   // if one rule fails: fail all
+
       if ($after.pos > $before.pos || $after.sp > $before.sp) {
         // Prevent adding whitespace if matched an optional rule last
         // Consequently All will fail if all the rules don't make any progress and don't put anything on stack
