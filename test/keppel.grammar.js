97b879340dccffdfe7dda5fb6d0abbd81ea09b09
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2022-10-01 20:05:47 +0900

    get rid of Y in the test Keppel grammar

--- test/keppel.grammar.js
+++ test/keppel.grammar.js
@@ -1,4 +1,4 @@
-import { Ignore, All, Any, Plus, Optional, Node, Y } from '../src/index.js';
+import { Ignore, All, Any, Plus, Optional, Node } from '../src/index.js';
 
 const Identifier = /^([a-zA-Z][a-zA-Z0-9_-]*)/;
 const Text = Any(
@@ -7,24 +7,24 @@ const Text = Any(
 );
 
 // Ignore line comments and all whitespace
-const Keppel = Ignore(/^\s+|^\/\/[^\r\n]*\n/, Y(ThisGrammar => {
 
-  const TagAttr = Node(All(Identifier, '=', Text), ([name, value]) => ({name, value}));
-  const TagAttrBlock = Node(All('(', TagAttr, Optional(Plus(All(',', TagAttr))), ')'), stack => ({attributes: stack}));
-  const TagId = Node(All('#', Identifier), ([id]) => ({id}));
-  const TagClasses = Node(Plus(All('.', Identifier)), stack => ({classes: stack}));
+const ThisGrammar = $ => Keppel($);
 
-  const TagHeader = Node(All(Identifier, Optional(TagAttrBlock), Optional(TagId), Optional(TagClasses)),
-    ([name, ...rest]) => rest.reduce((acc, e) => Object.assign(acc, e), {name}));
+const TagAttr = Node(All(Identifier, '=', Text), ([name, value]) => ({name, value}));
+const TagAttrBlock = Node(All('(', TagAttr, Optional(Plus(All(',', TagAttr))), ')'), stack => ({attributes: stack}));
+const TagId = Node(All('#', Identifier), ([id]) => ({id}));
+const TagClasses = Node(Plus(All('.', Identifier)), stack => ({classes: stack}));
 
-  const TagBody = Node(All('[', ThisGrammar, ']'), ([body]) => ({body}));
+const TagHeader = Node(All(Identifier, Optional(TagAttrBlock), Optional(TagId), Optional(TagClasses)),
+  ([name, ...rest]) => rest.reduce((acc, e) => Object.assign(acc, e), {name}));
 
-  const Tag = Node(All(TagHeader, Optional(TagBody)),
-    ([header, body]) => Object.assign({ type: 'tag', ...header, }, body || {}));
+const TagBody = Node(All('[', ThisGrammar, ']'), ([body]) => ({body}));
 
-  const FreeText = Node(Text, ([value]) => ({ type: 'free text', value }));
+const Tag = Node(All(TagHeader, Optional(TagBody)),
+  ([header, body]) => Object.assign({ type: 'tag', ...header, }, body || {}));
 
-  return Node(Plus(Any(Tag, FreeText)), stack => stack);
-}));
+const FreeText = Node(Text, ([value]) => ({ type: 'free text', value }));
 
-export default Keppel;
+const Keppel = Node(Plus(Any(Tag, FreeText)), stack => stack);
+
+export default Ignore(/^\s+|^\/\/[^\r\n]*\n/, ThisGrammar);
