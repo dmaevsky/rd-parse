260a68f3693ef3cc646ab8f3468bcfe8f580c285
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2022-11-12 18:07:06 +0900

    rename test -> tests

--- tests/keppel.spec.js
+++ tests/keppel.spec.js
@@ -18,8 +18,8 @@ test('Non-string input throws', () => {
 });
 
 test('Keppel grammar', async t => {
-  const example1 = fs.readFileSync(`${process.cwd()}/test/test1.kppl`, {encoding: 'utf-8'});
-  const example2 = fs.readFileSync(`${process.cwd()}/test/test2.kppl`, {encoding: 'utf-8'});
+  const example1 = fs.readFileSync(`${process.cwd()}/tests/test1.kppl`, {encoding: 'utf-8'});
+  const example2 = fs.readFileSync(`${process.cwd()}/tests/test2.kppl`, {encoding: 'utf-8'});
 
   await snapshot(parser(example1), t.name);
   await snapshot(parser(example2), t.name);
