b7bcfd24c01e4d7b5f6c347a9560bc4f9bb976ee
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2022-11-12 18:05:33 +0900

    ava -> node:test + utap + usnap

--- test/keppel.spec.js
+++ test/keppel.spec.js
@@ -1,21 +1,26 @@
-import test from 'ava';
+import test from 'node:test';
+import assert from 'node:assert/strict';
+
 import fs from 'fs';
+import snapshot from 'usnap';
+
+snapshot.setup(import.meta.url);
 
 import Parser from '../src/index.js';
 import Grammar from './keppel.grammar.js';
 
 const parser = Parser(Grammar);
 
-test('Non-string input throws', t => {
-  t.throws(() => parser({ text: 'text' }), {
-    message: 'Parsing function expects a string input'
-  });
+test('Non-string input throws', () => {
+  assert.throws(() => parser({ text: 'text' }),
+    new Error('Parsing function expects a string input')
+  );
 });
 
-test('Keppel grammar parser generation', t => {
+test('Keppel grammar', async t => {
   const example1 = fs.readFileSync(`${process.cwd()}/test/test1.kppl`, {encoding: 'utf-8'});
   const example2 = fs.readFileSync(`${process.cwd()}/test/test2.kppl`, {encoding: 'utf-8'});
 
-  t.snapshot(parser(example1));
-  t.snapshot(parser(example2));
+  await snapshot(parser(example1), t.name);
+  await snapshot(parser(example2), t.name);
 });
